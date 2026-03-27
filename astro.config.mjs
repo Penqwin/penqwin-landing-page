18aef283ed6673b3c9e14fccd4bc396599ab8fb0
sarin <sarinmsari@gmail.com>
2026-03-27 13:22:05 +0530

    site url update with www

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://penqwin.com',
+    site: 'https://www.penqwin.com',
     trailingSlash: 'never',
     integrations: [sitemap()],
     vite: {
